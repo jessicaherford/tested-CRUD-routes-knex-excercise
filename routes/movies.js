326e4418bacea5010b98baa2474b678da7938aee
JessicaHerford <jrherford12@gmail.com>
2016-01-19 15:48:08 -0700

    Got test_movie_reviews_routes.js passing

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('../db/knex');
 /* Movies Routes Go Here. */
 
+
+function movies(){
+  // console.log('**********');
+  // console.log(knex('restaurants'));
+  // console.log(knex('food'));
+  return knex('movies');
+}
+
 router.get('/', function (req, res, next) {
-  res.render('movies/index');
+  movies().select().then(function(results){
+  res.render('movies/index', {movies: results});
+  });
 })
+
+router.get('/:movie_id/reviews', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+
+router.get( '/:movie_id/reviews/new', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+
+router.post( '/:movie_id/reviews', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+router.get( '/:movie_id/reviews/:id', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+router.get( '/:movie_id/reviews/:id/edit', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+router.post( '/:movie_id/reviews/:id', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+router.post( '/:movie_id/reviews/:id/delete', function (req, res, next) {
+  movies().select().then(function(results){
+  res.render('movies/show', {movies: results});
+  });
+})
+
+
 
 module.exports = router;
