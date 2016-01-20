12296cbbab45e38ad8d2d6e856838c96d0b272da
JessicaHerford <jrherford12@gmail.com>
2016-01-19 19:32:05 -0700

    All tests passing

--- routes/movies.js
+++ routes/movies.js
@@ -3,8 +3,47 @@ var router = express.Router();
 
 /* Movies Routes Go Here. */
 
+var knex = require('../db/knex');
+
+
+function movies(){
+  // console.log('**********');
+  // console.log(knex('restaurants'));
+  // console.log(knex('food'));
+  return knex('movies');
+}
+
+function Reviews(){
+  return knex('reviews');
+}
+
 router.get('/', function (req, res, next) {
   res.render('movies/index');
+})
+
+
+router.get('/new', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.post('/', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:id', function(req, res, next){
+  res.render('movies/show');
+})
+
+router.get('/:id/edit', function(req, res, next){
+  res.render('movies/show');
+})
+
+router.post('/:id', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.post('/:id/delete', function (req, res, next) {
+  res.render('movies/show');
 })
 
 module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* Movies Routes Go Here. */
+
+router.get('/', function (req, res, next) {
+  res.render('movies/index');
+})
+
+router.get('/:movie_id/reviews', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:movie_id/reviews/new', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.post('/:movie_id/reviews', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:movie_id/reviews/:id', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:movie_id/reviews/:id/edit', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.post('/:movie_id/reviews/:id', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.post('/:movie_id/reviews/:id/delete', function (req, res, next) {
+  res.render('movies/show');
+})
+
+
+module.exports = router;
