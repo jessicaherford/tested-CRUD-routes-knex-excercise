64fd70674478bcaf40049476a48700cbba5bd7c9
JessicaHerford <jrherford12@gmail.com>
2016-01-19 19:45:34 -0700

    Heroku

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-/* Movies Routes Go Here. */
 
 router.get('/', function (req, res, next) {
   res.render('movies/index');
@@ -37,77 +35,4 @@ router.post('/:movie_id/reviews/:id/delete', function (req, res, next) {
 })
 
 
-=======
-var knex = require('../db/knex');
-/* Movies Routes Go Here. */
-
-
-function movies(){
-  // console.log('**********');
-  // console.log(knex('restaurants'));
-  // console.log(knex('food'));
-  return knex('movies');
-}
-
-function reviews(){
-  // console.log('**********');
-  // console.log(knex('restaurants'));
-  // console.log(knex('food'));
-  return knex('reviews');
-}
-
-
-router.get('/', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/index', {movies: results});
-  });
-})
-
-router.get('/:movie_id/reviews', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-
-router.get( '/:movie_id/reviews/new', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-
-router.post( '/:movie_id/reviews', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-router.get( '/:movie_id/reviews/:id', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-router.get( '/:movie_id/reviews/:id/edit', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-router.post( '/:movie_id/reviews/:id', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-router.post( '/:movie_id/reviews/:id/delete', function (req, res, next) {
-  movies().select().then(function(results){
-  res.render('movies/show', {movies: results});
-  });
-})
-
-
-
->>>>>>> 8973db64949ed3a6af98b6cb55fdf6c30040d50e
 module.exports = router;
